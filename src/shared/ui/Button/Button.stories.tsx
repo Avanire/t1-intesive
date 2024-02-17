d5a125891480c32bb01841cf51df5665a61b2069
Avanire <drtony@yandex.ru>
2024-02-17 09:03:26 +0600

    storybook

--- src/shared/ui/Button/Button.stories.tsx
+++ src/shared/ui/Button/Button.stories.tsx
@@ -2,10 +2,13 @@ import type { Meta, StoryObj } from '@storybook/react'
 import { Button } from './Button'
 import '../../../app/index.css'
 import arrowIcon from '../../../assets/images/Arrow.svg'
+import arrowLeftIcon from '../../../assets/images/arrow-left.svg'
+import { withRouter } from 'storybook-addon-react-router-v6'
 
 const meta: Meta<typeof Button> = {
     component: Button,
     title: 'shared/ui/Button',
+    decorators: [withRouter],
 }
 
 export default meta
@@ -30,5 +33,15 @@ export const WithIcon: Story = {
         children: 'Click me',
         theme: 'WithIcon',
         icon: arrowIcon,
+    },
+}
+
+export const ButtonLikeLink: Story = {
+    args: {
+        children: 'Click me',
+        icon: arrowLeftIcon,
+        iconDirect: 'ltr',
+        theme: 'Secondary',
+        type: 'link',
     },
 }
